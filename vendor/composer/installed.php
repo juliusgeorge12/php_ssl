49002e7e37cfe5c4a2d7fa9a992a96e68b7a8c6a
julius george <julius.business12@gmail.com>
2023-01-10 11:55:35 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'george/php_ssl',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2d87399d5b0850618b5e2bd417b829aa74ac0ae3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'george/php_ssl' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2d87399d5b0850618b5e2bd417b829aa74ac0ae3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
